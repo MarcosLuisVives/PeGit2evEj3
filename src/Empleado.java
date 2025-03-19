91b8979f8c814450c60475f1f0e9943f6e6276c6
MarcosLuisVives <marcos.contreras@alumno.iesluisvives.org>
2025-03-19 14:27:39 +0100

    ver 3

--- src/Empleado.java
+++ src/Empleado.java
@@ -3,5 +3,8 @@ public class Empleado {
     private String nombre;
     private int edad;
     private double sueldo;
+    void incrementarSueldo(int porcentajeIncremento) {
+        this.sueldo += sueldo * (porcentajeIncremento/100.0);
+    }
 
 }
