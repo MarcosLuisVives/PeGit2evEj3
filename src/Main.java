efb6b7f8486b9665ed53c24a496a959765ad8f87
MarcosLuisVives <marcos.contreras@alumno.iesluisvives.org>
2025-03-19 14:14:41 +0100

    version 1 quitando errores

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-    int numero;
+    int numero=6;
     String nombre="marcos";
+    int suma=numero+numero;
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
